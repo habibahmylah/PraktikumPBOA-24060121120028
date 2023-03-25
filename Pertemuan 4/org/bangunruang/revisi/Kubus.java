9ca0c60b28a68dd509656172b0403c2fdc0a9803
Habibah Mylah Dalilah <100356740+habibahmylah@users.noreply.github.com>
2023-03-25 11:22:47 +0700

    Add files via upload

---
+++ Pertemuan 4/org/bangunruang/revisi/Kubus.java
@@ -0,0 +1,28 @@
+/**
+* Nama File : Kubus.java  15/03/2023
+* Nama Penulis : Habibah Mylah Dalilah
+* NIM : 24060121120028
+* Deskripsi : Kubus
+*
+*/
+
+package org.bangunruang;
+
+import org.bangundatar.BujurSangkar;
+
+public class Kubus{
+	private BujurSangkar permukaan;
+	
+	public Kubus(BujurSangkar permukaan){
+		this.permukaan = permukaan;
+	}
+	
+	public double hitungLuasAlas(){
+		return permukaan.hitungLuas();
+	}
+	
+	public double hitungVolume(){
+		double sisi = permukaan.getSisi();
+		return sisi * sisi * sisi;
+	}
+}
