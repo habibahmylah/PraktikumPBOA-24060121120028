c8365a1db088d4a7b2b209ed72d74f5611fb500e
Habibah Mylah Dalilah <100356740+habibahmylah@users.noreply.github.com>
2023-04-12 20:50:55 +0700

    Delete Pertemuan 6 directory

--- Pertemuan 6/StokException.java
+++
@@ -1,7 +0,0 @@
-public class StokException extends Exception {
-    public StokException(Barang barang){
-        // TODO 5
-		super("Stok barang "+ barang.getNamaBarang() + "(" + barang.getIdBarang() + ")" + "Tidak Cukup!");
-    }
-}
-
